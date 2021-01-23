67c194b36c454d4d6837a4f1d5070ddb3f20e7ea
Redeat Bekele <71238188+redeat17@users.noreply.github.com>
2021-01-23 00:00:57 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1,49 +1,54 @@
 // from data.js
-var tableData = data;
-console.log(data);
-// YOUR CODE HERE!
-var tbody = d3.select("#ufo-table>tbody");
-console.log(tbody.node());
-// Dynamically build table
-/*tableData.forEach(record => {
-    var row = tbody.append("tr");
+const tableData = data;
 
-    row.append("td").text(record["datetime"]);
-    row.append("td").text(record["city"]);
-    row.append("td").text(record["state"]);
-    row.append("td").text(record["country"]);
-    row.append("td").text(record["shape"]);
-    row.append("td").text(record["durationMinutes"]);
-    row.append("td").text(record["comments"]);
-})
-*/
-tableData.forEach((ufoSite) => {
-    var row = tbody.append("tr");
-    Object.entries(ufoSite).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+
+const table = d3.select('table');
+const tableBody = table.select('tbody');
+const filterBtn = d3.select('#filter-btn');
+
+// Call the create table function to build the table and insert data into the table.
+createTable(tableData, tableBody);
+
+// State to hold the values of the user input fields.
+let inputValues = {
+  datetime: '',
+};
+
+// Get the keys of the input elements.
+// and use those keys to set the name attribute for each input element.
+const inputKeys = Object.keys(inputValues);
+d3.selectAll('.form-control').each(function(d, i) {
+  this.setAttribute('name', inputKeys[i]);
+});
+
+// Function that gets fired when value of input changes.
+d3.selectAll('.form-control').on('change', event => (inputValues[d3.event.target.name] = d3.event.target.value));
+
+// Event listener that gets fired when the filter button is clicked.
+filterBtn.on('click', () => {
+  // filterValues are the values of what the user typed inside the form input fields.
+  const filterValues = Object.values(inputValues);
+  const tableRows = tableBody.selectAll('tr');
+  tableRows.each(function() {
+    let row = this;
+    // Display each row initially.
+    row.style.display = '';
+    // Get the table data for each row.
+    let td = row.getElementsByTagName('td');
+    // Convert table data to an array so that we can iterate over it.
+    let tdArray = Array.from(td);
+    tdArray.forEach(function(td, tdIndex) {
+      let cell = row.getElementsByTagName('td')[tdIndex];
+      if (cell) {
+        if (filterValues[tdIndex]) {
+          // If the row is not already filtered out and matches the filter from the user input,
+          // then display the row.
+          if (row.style.display !== 'none' && cell.innerHTML.toUpperCase().indexOf(filterValues[tdIndex].toUpperCase()) > -1) {
+            return;
+          }
+          row.style.display = 'none';
+        }
+      }
     });
   });
-
-Object.values(record).forEach(col => {
-    row.append("td").text(col);
 });
-
-//Add event listener to button
-btn = d3.select("#filter-btn");
-btn.on("click", function test(){
-    tbody.html("");
-
-var inputDate = d3.select("#datetime").property("value");
-var filteredData = tableData.filter(row => row.datetime === inputDate);
-
-filteredData.forEach((ufoSite) => {
-  var row = tbody.append("tr");
-  Object.entries(ufoSite).forEach(([key, value]) => {
-    var cell = row.append("td");
-    cell.text(value);
-  });
-});
-
-})
-
